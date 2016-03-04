9b409cfb191b354e93d03ef5290bd8538fb4daba
Nickta Rawieo <rawieo@yandex.ru>
2016-03-05 02:39:08 +0400

    -_-

--- build_coffee.py
+++
@@ -1,7 +0,0 @@
-from os import system
-
-coffee_sources = ['master']
-
-if __name__ == '__main__':
-    for coffee_source in coffee_sources:
-        system('coffee -o js/ -c coffee/{filename}.coffee'.format(filename=coffee_source))
--- build_scss.py
+++
@@ -1,7 +0,0 @@
-from os import system
-
-scss_sources = ['master']
-
-if __name__ == '__main__':
-    for scss_source in scss_sources:
-        system('sass --scss scss/{filename}.scss css/{filename}.css'.format(filename=scss_source))
